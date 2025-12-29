49ff790ac84226a3c9b189eaf039f3305966306f
PedroRevesMD <pedroreves11@gmail.com>
2025-12-29 10:23:15 -0300

    First Challenge!

---
+++ 000.cpp
@@ -0,0 +1,13 @@
+// Square Problem
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long val = 327500;
+
+  long long ans = val * (4 * val * val - 1) / 3;
+
+  cout << ans << "\n";
+  return 0;
+}
